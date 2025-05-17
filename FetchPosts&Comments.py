63c2b5b6691e7c1a9ebec9692a236fddbdd2e5be
Elvin <elvinpaz672@gmail.com>
2025-05-16 23:11:17 -0500

    added subreddit instance

--- FetchPosts&Comments.py
+++ FetchPosts&Comments.py
@@ -11,9 +11,12 @@ USER_AGENT =os.getenv("USER_AGENT")
 import praw, pandas as pd
 
 reddit = praw.Reddit(
-    client_id = "CLIENT_ID",
-    client_secret = "CLIENT_SECRET",
-    user_agent = "USER_AGENT"
+    client_id = CLIENT_ID,
+    client_secret = CLIENT_SECRET,
+    user_agent = USER_AGENT
 )
 
-print(reddit.read_only)
+subreddit = reddit.subreddit("formula1")
+
+for post in subreddit.hot(limit = 10):
+    print(f"{post.title} [{post.score} votes]")
